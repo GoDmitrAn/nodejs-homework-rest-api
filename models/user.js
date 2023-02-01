73d966abe147a59efffe0414ab6f2486993ce0af
Dmytro Holombytsia <iqbitsak@gmail.com>
2023-02-01 22:49:35 +0200

    add user-model

---
+++ models/user.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const schema = mongoose.Schema(
+  {
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minLength: [4, " min pwd length must be 4 characters"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      match: [/[a-z0-9]+@[a-z0-9]+/, "user email is not valid"], //simple check
+      unique: true,
+    },
+    subscription: {
+      type: String,
+      enum: ["starter", "pro", "business"],
+      default: "starter",
+    },
+    token: {
+      type: String,
+      default: null,
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
+const User = mongoose.model("user", schema);
